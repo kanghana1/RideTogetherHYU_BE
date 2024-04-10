81e25ab260b41d48b691fea2506242eb67d5cb10
kanghana1 <qazplm12093@naver.com>
2024-04-10 23:36:49 +0900

    feat : 유저서비스 개발

--- src/main/java/com/ridetogether/server/domain/report/application/UserReportService.java
+++ src/main/java/com/ridetogether/server/domain/report/application/UserReportService.java
@@ -1,9 +1,13 @@
 package com.ridetogether.server.domain.report.application;
 
+import com.ridetogether.server.domain.member.dao.MemberRepository;
+import com.ridetogether.server.domain.member.domain.Member;
 import com.ridetogether.server.domain.report.Model.HandleStatus;
 import com.ridetogether.server.domain.report.dao.ReportRepository;
 import com.ridetogether.server.domain.report.domain.Report;
 import com.ridetogether.server.domain.report.dto.ReportDto;
+import com.ridetogether.server.domain.report.dto.ReportRequestDto;
+import com.ridetogether.server.domain.report.dto.ReportResponseDto;
 import com.ridetogether.server.global.apiPayload.code.status.ErrorStatus;
 import com.ridetogether.server.global.apiPayload.exception.handler.ErrorHandler;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static com.ridetogether.server.domain.report.dto.ReportDto.*;
+import static com.ridetogether.server.domain.report.dto.ReportRequestDto.*;
+import static com.ridetogether.server.domain.report.dto.ReportResponseDto.*;
 
 @Service
 @Transactional
@@ -20,7 +28,7 @@ import static com.ridetogether.server.domain.report.dto.ReportDto.*;
 public class UserReportService {
 
     private final ReportRepository reportRepository;
-
+    private final MemberRepository memberRepository;
     /**
      * 신고 등록
      * 본인이 한 신고 내역 조회
@@ -52,5 +60,49 @@ public class UserReportService {
         return report;
     }
 
+     // 특정 멤버가 작성한 거 가져오고 싶은데 .... @백도현오빠
+    public List<Report> getMyReports(Member member) {
+
+        List<Report> reportList = reportRepository.findAllByReporter(member);
+        if (reportList.isEmpty()) {
+            throw new ErrorHandler(ErrorStatus.REPORT_NOT_FOUND);
+        }
+        return reportList;
+    }
+
+    public ReportDetailInfoResponseDto getMyReportDetail(Long reportIdx) {
+        Report report = reportRepository.findByIdx(reportIdx)
+                .orElseThrow(() -> new ErrorHandler(ErrorStatus.REPORT_NOT_FOUND));
+
+        return ReportDetailInfoResponseDto.builder()
+                .reported(report.getReported())
+                .reportTitle(report.getReportTitle())
+                .reportContent(report.getReportContent())
+                .images(report.getImages())
+                .build();
+
+    }
+
+    public ReportInfoResponseDto updateReport(Report updatedreport) {
+        Report originReport = reportRepository.findByIdx(updatedreport.getIdx())
+                .orElseThrow(() -> new ErrorHandler(ErrorStatus.REPORT_NOT_FOUND));
+
+        originReport.updateReport(updatedreport);
+        return ReportInfoResponseDto.builder()
+                .idx(originReport.getIdx())
+                .reportTitle(originReport.getReportTitle())
+                .reportContent(originReport.getReportContent())
+                .images(originReport.getImages())
+                .build();
+    }
+
+    public ReportDeleteResponseDto deleteReport(Report report) {
+        Report deleteReport = reportRepository.findByIdx(report.getIdx()).orElseThrow(() -> new ErrorHandler(ErrorStatus.REPORT_NOT_FOUND));
+        reportRepository.deleteById(deleteReport.getIdx());
+
+        return ReportDeleteResponseDto.builder()
+                .isSuccess(true)
+                .build();
+    }
 
 }
