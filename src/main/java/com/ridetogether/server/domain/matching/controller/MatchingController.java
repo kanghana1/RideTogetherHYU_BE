aaf2d3b96df2714c6e09dec817b541bb60da84c3
dh1010a <dh1010a@naver.com>
2024-04-21 18:37:04 +0900

    feat: 매칭 생성 api 구현

--- src/main/java/com/ridetogether/server/domain/matching/controller/MatchingController.java
+++ src/main/java/com/ridetogether/server/domain/matching/controller/MatchingController.java
@@ -1,4 +1,33 @@
 package com.ridetogether.server.domain.matching.controller;
 
+import com.ridetogether.server.domain.matching.application.MatchingService;
+import com.ridetogether.server.domain.matching.converter.MatchingDtoConverter;
+import com.ridetogether.server.domain.matching.dto.MatchingRequestDto;
+import com.ridetogether.server.domain.member.domain.Member;
+import com.ridetogether.server.global.apiPayload.ApiResponse;
+import com.ridetogether.server.global.apiPayload.code.status.ErrorStatus;
+import com.ridetogether.server.global.apiPayload.exception.handler.ErrorHandler;
+import com.ridetogether.server.global.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.ridetogether.server.domain.matching.dto.MatchingRequestDto.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/matching")
 public class MatchingController {
+
+    private final MatchingService matchingService;
+
+    @PostMapping
+    public ApiResponse<?> createMatching(@RequestBody CreateMatchingRequestDto requestDto) {
+        Member loginMember = SecurityUtil.getLoginMember()
+                .orElseThrow(() -> new ErrorHandler(ErrorStatus.MEMBER_NOT_FOUND));
+        return ApiResponse.onSuccess(matchingService.createMatching(MatchingDtoConverter.convertToCreateMatchingDto(requestDto, loginMember.getIdx())));
+    }
+
 }
--- src/main/java/com/ridetogether/server/domain/member/model/PayType.java
+++ src/main/java/com/ridetogether/server/domain/member/model/PayType.java
@@ -1,5 +1,11 @@
 package com.ridetogether.server.domain.member.model;
 
+import static java.util.Locale.ENGLISH;
+
 public enum PayType {
-	KAKAO_PAY, TOSS_PAY, ACCOUNT_PAY
+	KAKAO_PAY, TOSS_PAY, ACCOUNT_PAY;
+
+	public static PayType fromName(String type) {
+		return PayType.valueOf(type.toUpperCase(ENGLISH) + "_PAY");
+	}
 }
